b95d8a47c2b6230bcac0046073b0d86e57cca6cc
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-01-10 16:52:47 +0600

    Added Ceaser.Cipher and Decipher

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Caesar = require('caesar-salad').Vigenere;
+const Caesar = require('caesar-salad').Caesar;
 
 const app = express();
 const port = 8000;
@@ -10,8 +10,18 @@ app.get('/:text', (req, res) => {
     res.send(req.params.text);
 });
 
+app.get('/encode/:text', (req, res) => {
+    const inputText = req.params.text;
+    const encodedText = Caesar.Cipher(password).crypt(inputText);
+    res.send(encodedText);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const inputText = req.params.text;
+    const decodedText = Caesar.Decipher(password).crypt(inputText);
+    res.send(decodedText);
+});
+
 app.listen(port, () => {
     console.log('Server online on ' + port);
 });
-
-
