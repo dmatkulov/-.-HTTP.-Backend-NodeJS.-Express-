971aac4308c92ba9f037fdc23c1d9d8e94692938
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2024-01-10 16:38:00 +0600

    Installed Ceaser cipher library

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const Caesar = require('caesar-salad').Vigenere;
+
 const app = express();
 const port = 8000;
 
-app.get('/:echo', (req, res) => {
-    res.send(req.params.echo);
+const password = 'password';
+
+app.get('/:text', (req, res) => {
+    res.send(req.params.text);
 });
 
 app.listen(port, () => {
     console.log('Server online on ' + port);
 });
+
+
